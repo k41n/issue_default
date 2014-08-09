9566ead62d27e50751e25c52a6e7e89ad837ed08
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2014-08-09 23:41:07 +0400

    Version bump

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :issue_defaults do
   name 'Issue Defaults plugin'
   author 'LLC Kodep'
   description 'Customization in accordance to Stas Malyshev requirements'
-  version '0.0.3'
+  version '1.0.0'
   url 'https://github.com/k41n/issue_default'
   author_url 'Andrei.Malyshev@kodep.ru'
   
