1e25425218782278015aa022389b2de32128672f
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2014-08-09 17:00:40 +0400

    Escalation refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-post '/issues/:id/escalate', action: :escalate, controller: :issues, as: :escalate
+put '/issues/:id/escalate', action: :escalate, controller: :issues, as: :escalate
--- lib/patches/issues_controller_patch.rb
+++ lib/patches/issues_controller_patch.rb
@@ -14,13 +14,15 @@ module RedmineIssueDefaults
       module InstanceMethods
         def escalate
           find_issue
+          update
           head 403 && return unless User.current.roles_for_project(@issue.project).map(&:name).include?('Инициатор')
+          Rails.logger.info "-------- ESCALATION !!! ------------"
           User.find_each do |user|
             if user.roles_for_project(@issue.project).map(&:name).include?('Эскалатор')
               Watcher.create(:watchable => @issue, :user_id => user.id)
             end
           end
-          redirect_to issue_path(@issue)
+          # redirect_to issue_path(@issue)
         end
       end
 
