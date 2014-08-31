99022d6211c8dc9fcd57c926f4ce7548bea877f3
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2014-08-31 14:20:24 +0400

    (#6) Track changes of the status only in issue journal

--- lib/patches/issue_patch.rb
+++ lib/patches/issue_patch.rb
@@ -3,11 +3,38 @@ module RedmineIssueDefaults
 
     module IssuePatch
       def self.included(base) # :nodoc:
+        base.send(:include, InstanceMethods)
+
         base.class_eval do
           safe_attributes :corellation_act_type
+
+          alias_method_chain :create_journal, :sber
         end
       end
     end
+
+    module InstanceMethods
+      def create_journal_with_sber
+        if @current_journal
+          # attributes changes
+          if @attributes_before_change
+            %w(status_id).each {|c|
+              before = @attributes_before_change[c]
+              after = send(c)
+              next if before == after || (before.blank? && after.blank?)
+              @current_journal.details << JournalDetail.new(:property => 'attr',
+                                                            :prop_key => c,
+                                                            :old_value => before,
+                                                            :value => after)
+            }
+          end
+          @current_journal.save
+          # reset current journal
+          init_journal @current_journal.user, @current_journal.notes
+        end
+      end
+    end
+
   end
 end
 
