b59c553234a9513144ab17925215bf4bc49b37b0
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2014-09-02 23:16:52 +0400

    (#15) Added file presence validation for another rare case

--- lib/hooks/issues_controller_hooks.rb
+++ lib/hooks/issues_controller_hooks.rb
@@ -7,6 +7,9 @@ module RedmineIssueDefaults
         if ['Отмена операции', 'Акты сверки'].include?(issue.tracker.name) && params[:attachments].blank?
           issue.attachment_error = 'Так дело не пойдет, надо приложить файл'
         end
+        if issue.tracker.name == 'Розыск платежа' && ['Розыск по транзакции', 'Розыск неопознанной суммы'].include?(params[:issue][:custom_field_values].try(:[],'34')) && params[:attachments].blank?
+          issue.attachment_error = 'Так дело не пойдет, надо приложить файл'
+        end
       end
     end
   end
